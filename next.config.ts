937a0733597d01f2c3adebae7b17e3fd185707e0
ayushsuperapi <ayush.ranjan@trysuperapi.com>
2025-06-24 12:58:29 +0530

    update next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@
 const nextConfig = {
   async rewrites() {
     return {
-      beforeFiles: [
+      afterFiles: [
         {
           source: '/dashboard',
           destination: 'https://platform.superapi.cloud/app',
